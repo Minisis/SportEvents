b8f9fe2116220ae85b49bf06cd9a1f16a4c5a244
lvaldovinos <lastkiss115@gmail.com>
2014-10-19 13:43:33 -0700

    Adding CORS...

--- router/index.js
+++ router/index.js
@@ -8,6 +8,17 @@ var router = function(app) {
 	app.get('/', function(req, res) {
 		res.send('<h1>Hello bros...</h1>');
 	});
+	app.use(function(req, res, next){
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
+		if (req.method === 'OPTIONS') {
+			res.send();	
+		}
+		else {
+			next();
+		}	
+	});
 	app.get('/load', loadRouter.get);
 	app.use('/venue', venueRouter);
 	app.use('/event', eventRouter);
